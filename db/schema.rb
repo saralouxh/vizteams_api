bade9a173ce8bd28d936404d22f2edec41c90d6f
Sara Hampson <shampson518@gmail.com>
2023-05-01 18:30:20 -0500

    update team again

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_01_225458) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_01_232819) do
+  create_table "team_members", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
