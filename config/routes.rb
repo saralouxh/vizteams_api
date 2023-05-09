125d816a72358c2bc0d4b638048f9a465e55a5ac
Draike13 <tmbiri91@gmail.com>
2023-05-08 19:30:20 -0500

    set up auth with JWT
    Co-authored-by: Heath Falkenrath <theGeekkit@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  resources :users, param: :_id
+  post '/auth/login', to: 'authentication#login'
+  get '/*a', to: 'application#not_found'
+
+
   resources :team_members
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :users
 end
