b405c4f2c0a381a3cdef0b06f3c78d51dd064d1b
Joe Letner <jrletner@gmail.com>
2023-05-13 14:30:10 -0500

    updated for payload json

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,21 +1,27 @@
 class AuthenticationController < ApplicationController
-    before_action :authorize_request, except: :login
-  
-    # POST /auth/login
-    def login
-      @user = User.find_by_email(params[:email])
-      if @user&.authenticate(params[:password])
-        token = JsonWebToken.encode(user_id: @user.id)
-        time = Time.now + 24.hours.to_i
-        render json: { token: token, exp: time.strftime("%m-%d-%Y %H:%M")}, status: :ok
-      else
-        render json: { error: 'unauthorized' }, status: :unauthorized
-      end
-    end
-  
-    private
-  
-    def login_params
-      params.permit(:email, :password)
+  before_action :authorize_request, except: :login
+
+  # POST /auth/login
+  def login
+    @user = User.find_by_email(params[:email])
+    if @user&.authenticate(params[:password])
+      email = params[:email]
+      token = JsonWebToken.encode(user_id: @user.id)
+      time = Time.now + 24.hours.to_i
+      payload = {
+        email: params[:email],
+        token: token,
+        exp: time.strftime("%m-%d-%Y %H:%M"),
+      }
+      render json: { payload: payload }, status: :ok
+    else
+      render json: { error: "unauthorized" }, status: :unauthorized
     end
   end
+
+  private
+
+  def login_params
+    params.permit(:email, :password)
+  end
+end
