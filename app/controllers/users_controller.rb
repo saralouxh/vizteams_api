efb6090c7b32504dd5d25b2009b3fb6d1920b881
Draike13 <tmbiri91@gmail.com>
2023-05-18 19:43:53 -0500

    REadd the create user endpoint

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,16 @@ class UsersController < ApplicationController
     render json: @current_user, status: :ok
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user, status: :created
+    else
+      render json: { errors: @user.errors.full_messages },
+             status: :unprocessable_entity
+    end
+  end
+
   private
 
   def find_user
