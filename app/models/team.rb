ade4f58ab3f40a88b939a748f016b8089135b888
Draike13 <tmbiri91@gmail.com>
2023-05-24 14:55:13 -0500

    fix length of member array

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,6 +1,6 @@
 class Team < ApplicationRecord
     has_many :team_members, dependent: :destroy
-    validates_length_of :team_members, maximum: 12
+    validates_length_of :team_members, maximum: 11
     validates :name, presence: true, uniqueness: true
     validates :description, presence: true
 end
